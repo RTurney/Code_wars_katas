1c3d969c13368a39396920ec4453929eb5965415
RTurney <r.turney93@gmail.com>
2021-02-12 12:26:09 +0000

    add new spec. 1 tests red

--- wolfInSheepsClothing/spec/warnTheSheepSpec.js
+++ wolfInSheepsClothing/spec/warnTheSheepSpec.js
@@ -9,8 +9,12 @@ describe('Warn the sheep', () => {
 
   describe('will warn a sheep', () => {
 
-    it('if sheep is in front of the wolf', () => {
+    it('if sheep is in front of the wolf at the front', () => {
       expect(warnTheSheep(['wolf', 'sheep'])).toEqual("Oi! Sheep number 1! You are about to be eaten by a wolf");
+    });
+
+    it('if the sheep is in front of a wolf elsewhere', () => {
+      expect(warnTheSheep(['sheep', 'wolf', 'sheep', 'sheep'])).toEqual("Oi! Sheep number 2! You are about to be eaten by a wolf");
     });
   });
 });
