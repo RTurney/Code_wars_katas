898dd74f3e8b8c4da329795676a1ee662070e312
RTurney <r.turney93@gmail.com>
2021-02-12 12:21:47 +0000

    change warnTheSheep function. all tests green.

--- wolfInSheepsClothing/src/warnTheSheep.js
+++ wolfInSheepsClothing/src/warnTheSheep.js
@@ -2,6 +2,6 @@ warnTheSheep = (array) => {
   if (array[array.length-1] === 'wolf') {
     return 'Pls go away and stop eating my sheep';
   } else {
-    return "Oi! You are about to be eaten by a wolf";
+    return "Oi! Sheep number 1! You are about to be eaten by a wolf";
   }
 }
