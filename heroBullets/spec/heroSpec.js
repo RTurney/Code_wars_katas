114537f79cd382ae191563b5b56807b009ea3579
RTurney <r.turney93@gmail.com>
2021-02-12 11:17:12 +0000

    add second spec. 1 tests fail

--- heroBullets/spec/heroSpec.js
+++ heroBullets/spec/heroSpec.js
@@ -4,4 +4,8 @@ describe('Hero', () => {
     expect(hero(2, 1)).toEqual(true);
   });
 
+  it('will die if he does not have enough bullets', () => {
+    expect(hero(1, 1)).toEqual(false);
+  });
+
 });
