02a27ad8c5ebc62ce659292ba631f171eed5f6f7
RTurney <r.turney93@gmail.com>
2021-02-12 10:43:11 +0000

    Establish folder for Mean Of Arrays kata.

---
+++ MeanOfArray/spec/MeanOfArraysSpec.js
@@ -0,0 +1,10 @@
+describe('getAverage', () => {
+
+  describe('returns an average', () => {
+
+    it('will return 2', () => {
+      expect(getAverage([2, 2, 2, 2])).toEqual(2);
+    });
+  });
+
+});
