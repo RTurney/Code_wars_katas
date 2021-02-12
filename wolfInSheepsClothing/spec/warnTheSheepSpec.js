dd933e4202b577062ca51df92f2aa76dab9db72f
RTurney <r.turney93@gmail.com>
2021-02-12 12:15:07 +0000

    add second spec to warnTheSheep

--- wolfInSheepsClothing/spec/warnTheSheepSpec.js
+++ wolfInSheepsClothing/spec/warnTheSheepSpec.js
@@ -1,6 +1,16 @@
 describe('Warn the sheep', () => {
 
-  it('will plead with the wolf if the wolf is at the front', () => {
-    expect(warnTheSheep(["wolf"])).toEqual('Pls go away and stop eating my sheep');
+  describe('will plead with wolf', () => {
+
+    it('if the wolf is the only one in the array', () => {
+      expect(warnTheSheep(["wolf"])).toEqual('Pls go away and stop eating my sheep');
+    });
+  });
+
+  describe('will warn a sheep', () => {
+
+    it('if sheep is in front of the wolf', () => {
+      expect(warnTheSheep(['wolf', 'sheep'])).toEqual("Oi! You are about to be eaten by a wolf");
+    });
   });
 });
