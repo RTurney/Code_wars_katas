8a38ec935c833a23206eec9a6d3a18d879a4d834
RTurney <r.turney93@gmail.com>
2021-02-12 12:19:20 +0000

    refactor warnTheSheep. all tests green

--- wolfInSheepsClothing/src/warnTheSheep.js
+++ wolfInSheepsClothing/src/warnTheSheep.js
@@ -1,3 +1,7 @@
-warnTheSheep = () => {
-  return 'Pls go away and stop eating my sheep'
+warnTheSheep = (array) => {
+  if (array[array.length-1] === 'wolf') {
+    return 'Pls go away and stop eating my sheep';
+  } else {
+    return "Oi! You are about to be eaten by a wolf";
+  }
 }
