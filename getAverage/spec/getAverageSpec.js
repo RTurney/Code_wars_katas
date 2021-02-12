0ea48a7e06d809b50d3b461720bd1ead778053f2
RTurney <r.turney93@gmail.com>
2021-02-12 10:50:08 +0000

    add second spec. 1 test failing

--- getAverage/spec/getAverageSpec.js
+++ getAverage/spec/getAverageSpec.js
@@ -5,6 +5,10 @@ describe('getAverage', () => {
     it('will return 2', () => {
       expect(getAverage([2, 2, 2, 2])).toEqual(2);
     });
+
+    it('will return 3', () => {
+      expect(getAverage([1, 2, 3, 4, 5])).toEqual(3);
+    });
   });
 
 });
