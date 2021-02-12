0baebb2f011e81e3ff8e999ddea8f5322079d8ac
RTurney <r.turney93@gmail.com>
2021-02-12 10:54:06 +0000

    add for loop and average function to getAverage(). All tests green.

--- getAverage/spec/getAverageSpec.js
+++ getAverage/spec/getAverageSpec.js
@@ -10,5 +10,4 @@ describe('getAverage', () => {
       expect(getAverage([1, 2, 3, 4, 5])).toEqual(3);
     });
   });
-
 });
--- getAverage/src/getAverage.js
+++ getAverage/src/getAverage.js
@@ -1,3 +1,7 @@
-getAverage = () => {
-  return 2
+getAverage = (array) => {
+  let sum = 0;
+  for (let i = 0; i < array.length; i++) {
+    sum += array[i]
+  }
+  return sum/array.length;
 }
