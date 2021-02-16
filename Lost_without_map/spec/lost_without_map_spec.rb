239f3e95a8876dac96465d7435dadd41f8149eb0
RTurney <r.turney93@gmail.com>
2021-02-16 11:14:30 +0000

    Add spec. 1 test red

--- Lost_without_map/spec/lost_without_map_spec.rb
+++ Lost_without_map/spec/lost_without_map_spec.rb
@@ -3,6 +3,10 @@ require 'maps'
 describe 'maps' do
 
   it "will return an array" do
-    expect(maps([])).to eq [] 
+    expect(maps([])).to eq []
+  end
+
+  it "will return an array of [2, 2] for [1, 1]" do
+    expect(maps([1, 1])).to eq [2, 2]
   end
 end
