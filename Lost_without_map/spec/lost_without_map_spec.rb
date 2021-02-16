505cdc9dfb5f5a0f0a4708f707d3096543520c03
RTurney <r.turney93@gmail.com>
2021-02-16 11:18:08 +0000

    Add additional specs to lost_without_maps.rb. All tests green

--- Lost_without_map/spec/lost_without_map_spec.rb
+++ Lost_without_map/spec/lost_without_map_spec.rb
@@ -9,4 +9,8 @@ describe 'maps' do
   it "will return an array of [2, 2] for [1, 1]" do
     expect(maps([1, 1])).to eq [2, 2]
   end
+
+  it "will return an aray of [2, 4, 6] for [1, 2, 3]" do
+    expect(maps([1, 2, 3])).to eq [2, 4, 6]
+  end
 end
