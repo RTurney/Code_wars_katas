04c0c0a88f7311a09617906ce7e9c841ddade286
RTurney <r.turney93@gmail.com>
2021-02-19 12:13:46 +0000

    Pass three day test

--- Vactation_Transport/lib/rental_car_calculator.rb
+++ Vactation_Transport/lib/rental_car_calculator.rb
@@ -1,4 +1,8 @@
 
 def rental_car_calculator(days)
-  days * 40
+  if days >= 3
+    days * 40 - 20
+  else
+    days * 40
+  end
 end
--- Vactation_Transport/spec/rental_car_calculator_spec.rb
+++ Vactation_Transport/spec/rental_car_calculator_spec.rb
@@ -2,18 +2,19 @@ require 'rental_car_calculator'
 
 describe 'rental car calculator' do
 
-  describe 'will return' do
-
-    it '0 for 0 days' do
+    it 'will return 0 for 0 days' do
       expect(rental_car_calculator(0)).to eq 0
     end
 
-    it "40 for 1 day" do
+    it "will return 40 for 1 day" do
       expect(rental_car_calculator(1)).to eq 40
     end
 
-    it "80 for 2 days" do
+    it "will return 80 for 2 days" do
       expect(rental_car_calculator(2)).to eq 80
     end
-  end
+
+    it "will return 100 for 3 days" do
+      expect(rental_car_calculator(3)).to eq 100
+    end
 end
