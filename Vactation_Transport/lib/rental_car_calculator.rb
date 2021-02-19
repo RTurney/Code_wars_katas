f667e7c25d8ab02c8970928deb21f0c63603eeeb
RTurney <r.turney93@gmail.com>
2021-02-19 12:30:42 +0000

    Refactor rental_car_calculator. All tests green

--- Vactation_Transport/lib/rental_car_calculator.rb
+++ Vactation_Transport/lib/rental_car_calculator.rb
@@ -1,10 +1,8 @@
 
 def rental_car_calculator(days)
   if days < 3
-    days * 40
-  elsif days >= 7
-    days * 40 - 50
-  elsif days >= 3
-    days * 40 - 20
+    return days * 40
   end
+
+  days >= 7 ? (days * 40 - 50) : (days * 40 - 20)
 end
