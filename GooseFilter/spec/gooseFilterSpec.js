fec75c49a9b434b69e66931713a6360c79e31f87
RTurney <r.turney93@gmail.com>
2021-02-20 17:33:41 +0000

    Add gooseFiler.js. Pass all tests

--- GooseFilter/spec/gooseFilterSpec.js
+++ GooseFilter/spec/gooseFilterSpec.js
@@ -3,4 +3,5 @@ describe('Goose filter', () => {
   it('will return the passed in array', () => {
     expect(gooseFilter(['Duck'])).toEqual(['Duck']);
   });
+
 });
---
+++ GooseFilter/src/gooseFilter.js
@@ -0,0 +1,3 @@
+function gooseFilter(array) {
+  return array
+}
