0b672f589f1f792db77bcde3e44fa35f15431629
RTurney <r.turney93@gmail.com>
2021-02-26 09:27:30 +0000

    refactor switcheroo function. all tests green

--- Switcheroo/lib/switcheroo.rb
+++ Switcheroo/lib/switcheroo.rb
@@ -1,10 +1,15 @@
 
 def switcheroo(string)
-  if string == 'a'
-    'b'
-  elsif string == 'b'
-    'a'
-  else
-    string
+  switched_string = []
+  string.each_char do |chr|
+    if chr == 'a'
+      switched_string.push('b')
+    elsif chr == 'b'
+      switched_string.push('a')
+    else
+      switched_string.push(chr)
+    end
   end
+
+  switched_string.join
 end
