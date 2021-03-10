8953b5c065f8d73f920e6ea3232426b34e692070
RTurney <r.turney93@gmail.com>
2021-03-10 13:48:48 +0000

    Add transform function.

---
+++ ArrayToString/spec/transformSpec.js
@@ -0,0 +1,6 @@
+describe('Transform', () => {
+
+  it('will return "box" for ["box"]', () => {
+    expect(transform(["box"])).toEqual('box');
+  })
+})
---
+++ ArrayToString/src/transform.js
@@ -0,0 +1,3 @@
+function transform(){
+  return 'box'
+}
