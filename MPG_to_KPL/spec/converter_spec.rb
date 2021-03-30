a1be8796772cca83bc1300dff879e9f3395cb1a0
RTurney <r.turney93@gmail.com>
2021-03-30 15:38:16 +0100

    Checking online, the expected answers on kata may be wrong

--- MPG_to_KPL/spec/converter_spec.rb
+++ MPG_to_KPL/spec/converter_spec.rb
@@ -8,6 +8,10 @@ describe 'converter' do
   end
 
   it 'will return 1 for 2.35215' do
-    expect(converter(2.35215)).to eq 1
+    expect(converter(2.3521458)).to eq 1
+  end
+
+  it 'will return 3.54 for 10' do
+    expect(converter(12)).to eq 4.25
   end
 end
