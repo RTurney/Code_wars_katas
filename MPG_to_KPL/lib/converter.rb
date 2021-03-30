63c610cd1023437bf757ace3866e882dc2e1b205
RTurney <r.turney93@gmail.com>
2021-03-30 15:22:42 +0100

    Refactor converter function

--- MPG_to_KPL/lib/converter.rb
+++ MPG_to_KPL/lib/converter.rb
@@ -1,4 +1,5 @@
+# frozen_string_literal: true
 
 def converter(mpg)
-  0
+  mpg
 end
--- MPG_to_KPL/spec/converter_spec.rb
+++ MPG_to_KPL/spec/converter_spec.rb
@@ -1,8 +1,9 @@
+# frozen_string_literal: true
+
 require 'converter'
 
 describe 'converter' do
-
-  it "will return zero for zero" do
+  it 'will return zero for zero' do
     expect(converter(0)).to eq 0
   end
 end
