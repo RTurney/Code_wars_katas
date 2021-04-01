6aff89d6c9050d3c6ae9dcf5a95c830b1c0116bb
RTurney <r.turney93@gmail.com>
2021-04-01 10:18:38 +0100

    Add additional tests and edge case.

--- Repeater/spec/repeater.test.js
+++ Repeater/spec/repeater.test.js
@@ -9,4 +9,16 @@ describe('Repeater', () => {
   test('will return "aa" for ("a", 2)', () => {
     expect(repeater('a', 2)).toMatch('aa')
   });
+
+  test('will return "aaa" for ("a", 3)', () => {
+    expect(repeater("a", 3)).toMatch('aaa')
+  });
+
+  test('will return "wub" for ("wub", 1)', () => {
+    expect(repeater("wub", 1)).toMatch('wub')
+  });
+
+  test('will return nothing for ("wub", 0)', () => {
+    expect(repeater("wub", 0)).toMatch('')
+  });
 });
